c0e84391ae1b259d578636b363fd4b6ec79850a3
nunogbatista <nunogbatista06@gmail.com>
2025-08-18 16:40:12 +0000

    Webcam component version 1.1.3

---
+++ js/ts/youverse-webcam.d.ts
@@ -0,0 +1,33 @@
+declare namespace JSX {
+    interface IntrinsicElements {
+        'youverse-webcam': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+            cancelVerification?: boolean;
+            changeCamera?: boolean;
+            facePositionCheck?: boolean;
+            titleText?: string;
+            infoText?: string;
+            license?: string;
+            color?: string;
+            locale?: string;
+        };
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'youverse-webcam': YourverseWebcam;
+    }
+}
+
+interface YourverseWebcam extends HTMLElement {
+    cancelVerification?: boolean;
+    changeCamera?: boolean;
+    facePositionCheck?: boolean;
+    titleText?: string;
+    infoText?: string;
+    license?: string;
+    color?: string;
+    locale?: string;
+}
+
+export default YourverseWebcam;
